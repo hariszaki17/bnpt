05f82fe9e82cfbf25a84e615f8ea83c1ce2c6665
hariszaki17 <hariszaki17@gmail.com>
2024-06-27 10:14:11 +0700

    update controller

--- src/api/orang-kelompok-yang-berjasa/controllers/orang-kelompok-yang-berjasa.js
+++ src/api/orang-kelompok-yang-berjasa/controllers/orang-kelompok-yang-berjasa.js
@@ -6,4 +6,30 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::orang-kelompok-yang-berjasa.orang-kelompok-yang-berjasa');
+module.exports = createCoreController('api::orang-kelompok-yang-berjasa.orang-kelompok-yang-berjasa', ({ strapi }) => ({
+    async find(ctx) {
+        let res = await strapi.entityService.findMany('api::orang-kelompok-yang-berjasa.orang-kelompok-yang-berjasa', {
+            populate: '*',
+        });
+
+        let resp = [];
+        for (let i = 0; i < res.length; i++) {
+            if (res[i].publishedAt == null) {
+                continue
+            }
+            const data = res[i]
+            const obj = {
+                id: data.id,
+                nama: data.nama,
+                peristiwa: data.peristiwa,
+                meninggal_atau_luka: data.meninggal_atau_luka,
+                negara: data.negara,
+                narasi: data.narasi
+            }
+
+            resp.push(obj)
+        }
+
+        return { data: resp }
+    }
+}));
